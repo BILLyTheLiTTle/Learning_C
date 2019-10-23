3cdb60876dc1dc3ffe6715ffc43a4e2f6a0ab5b9
Vasilios Tsapalos <littleprog@gmail.com>
2019-10-23 12:45:13 +0300

    Example arithmetics with characters

---
+++ Chapter_1/1.6/arithmetics_with_characters.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  char c = 'C';
+  printf("%c - 1 = %c\n", c, c-1);
+  printf("%c + 1 = %c\n", c, c+1);
+  printf("%c + \'1\' = %c\n", c, c+'1');
+  printf("%c + \'\\1\' = %c\n", c, c+'\1');
+  return 0;
+}
