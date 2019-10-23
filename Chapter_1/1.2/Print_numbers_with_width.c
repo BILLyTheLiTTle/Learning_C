9ef5f1f93d9d65ae51536a04190b54d1016bc9ec
Vasilios Tsapalos <littleprog@gmail.com>
2019-10-23 15:58:57 +0300

    Improve examples

--- Chapter_1/1.2/Print_numbers_with_width.c
+++ Chapter_1/1.2/Print_numbers_with_width.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("%6.2d\n %8.2f\n %8.2f\n %d\n %8.2f\n", 1, 23.0, 33.3, 33333, 3333.33333);
+  printf("%6.2d\n" , 1);
+  printf("%8.2f\n", 23.0);
+  printf("%8.2f\n",33.3);
+  printf("%d\n",33333);
+  printf("%8.2f\n", 3333.33333);
+  printf("%8.4s", "Billy");
   return 0;
 }
--- Chapter_1/1.9/null_character.c
+++ Chapter_1/1.9/null_character.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <unistd.h>
-
+// See also here (https://stackoverflow.com/questions/58522758/termination-character-in-char-array)
 int main(int argc, char const *argv[]) {
-  char hello[6] = {'h', 'e', 'l', 'l', 'o', '\n'}; //without \0 prints some rubbish and the dot (why the dot?)
+  char hello[6] = {'h', 'e', 'l', 'l', 'o', '\n'}; //undefined behavior
   char bye[5] = {'b', 'y', 'e', '\n', '\0'};
   char end[] = "end";
   char dot = '.';
+  char oops[] = {'o', 'o', 'p', 's'}; //undefined behavior
 
   printf("%s\n", hello);
   printf("%s\n", bye);
   printf("%s\n", end);
-  printf("%s\n", dot);
+  printf("%s\n", dot); // undefined behavior (use %c instead of %s)
+  printf("%s\n", oops);
   return 0;
 }
