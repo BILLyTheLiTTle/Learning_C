7d80ae28f26b53b6a8e61a35596e662c02d80416
Vasilios Tsapalos <littleprog@gmail.com>
2019-10-29 17:01:38 +0200

    Visibility of macro substitution variables

---
+++ Chapter_04/4.03/Scenario_01/scenario_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "header_1.h" // Needs to be included no matter how I run it (F5 from Atom or g++ from command line)!
+
+int main(int argc, char const *argv[]) {
+  printf("%d\n", ONE);
+  return 0;
+}
