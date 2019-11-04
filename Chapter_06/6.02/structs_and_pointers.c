519da647e920bedb64a52ba3b87a99880e0c1848
Vasilios Tsapalos <littleprog@gmail.com>
2019-11-04 14:45:33 +0200

    Example of pointers and structures

---
+++ Chapter_06/6.02/structs_and_pointers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+struct person{
+  int age;
+  char *name;
+} p;
+
+struct person *pp = &p;
+
+int main(int argc, char const *argv[]) {
+  //struct person p = {5, "Bill"}; // Creates a new variable instead of the global above
+  p.age = 5;
+  p.name = "Bill";
+
+  printf("-- From structure itself --\n");
+  printf("Age: %d\n", p.age);
+  printf("Name: %s\n\n", p.name);
+
+  printf("-- From structure pointer --\n");
+  printf("Age [(*pp).age]: %d\n", (*pp).age); // 5
+  printf("Name [(*pp).name]: %s\n\n", (*pp).name); // Bill
+  printf("Age [pp->age]: %d\n", pp->age); // 5
+  printf("Name [pp->name]: %s\n\n", pp->name); // Bill
+  printf("First letter of name [*(*pp).name]: %c\n", (*pp).name); // B
+  printf("First letter of name [*((*pp).name)]: %c\n", *((*pp).name)); // B
+  printf("First letter of name [*pp->name]: %c\n", *pp->name); // B
+  printf("First letter of name [*(pp->name)]: %c\n", *(pp->name)); // B
+
+  //printf("First letter of name [*(pp->name)]: %c\n", *pp.*name); // Compile time error
+  return 0;
+}
